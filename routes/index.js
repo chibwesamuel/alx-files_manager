23707300e41fdba3b2d19224653377853f0f3ab1
chibwesamuel <smchibwe@gmail.com>
2023-09-06 09:38:19 +0000

    Task 3

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 // routes/index.js
 import express from 'express';
-import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 // Define routes here
-router.get('/', AppController.getHome);
-router.post('/add', AppController.addNewItem);
+router.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the API' });
+});
+
+// Add the new user creation endpoint
+router.post('/users', UsersController.postNew);
 
 export default router;
 
